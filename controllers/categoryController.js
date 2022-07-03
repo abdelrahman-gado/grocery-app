2906f2860c2c08ccbb2ad3b6538f65c043465809
abdelrahman <gadoua50@gmail.com>
2022-07-03 19:20:36 +0200

    define category controller

---
+++ controllers/categoryController.js
@@ -0,0 +1,34 @@
+const Category = require('../models/category');
+
+exports.category_list = function (req, res) {
+  res.send("NOT IMPLEMNETED");
+};
+
+exports.category_detail = function (req, res) {
+  res.send("NOT IMPLEMNETED");
+};
+
+exports.category_create_get = function (req, res) {
+  res.send("NOT IMPLEMNETED");
+};
+
+exports.category_create_post = function (req, res) {
+  res.send("NOT IMPLEMNETED");
+};
+
+exports.category_delete_get = function (req, res) {
+  res.send("NOT IMPLEMNETED");
+};
+
+exports.category_delete_post = function (req, res) {
+  res.send("NOT IMPLEMNETED");
+};
+
+exports.category_update_get = function (req, res) {
+  res.send("NOT IMPLEMNETED");
+};
+
+exports.category_update_post = function (req, res) {
+  res.send("NOT IMPLEMNETED");
+};
+
