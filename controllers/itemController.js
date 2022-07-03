7c4d86f1c51f2ca0162b6f388e3fb5f120d69522
abdelrahman <gadoua50@gmail.com>
2022-07-03 19:21:16 +0200

    define item controller

---
+++ controllers/itemController.js
@@ -0,0 +1,41 @@
+const Item = require('../models/item');
+
+exports.item_list = function (req, res) {
+  res.send('NOT IMPLEMENTED');
+};
+
+exports.item_detail = function (req, res) {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.item_create_get = function (req, res) {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.item_create_post = function (req, res) {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.item_delete_get = function (req, res) {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.item_delete_post = function (req, res) {
+  res.send("NOT IMPLEMENTED");
+};
+
+
+exports.item_update_get = function (req, res) {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.item_update_post = function (req, res) {
+  res.send("NOT IMPLEMENTED");
+};
+
+
+
+
+
+
+
