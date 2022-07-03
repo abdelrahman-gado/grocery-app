b6a7665c60d8075f92043d4f3c4d2ea3fa32ee75
abdelrahman <gadoua50@gmail.com>
2022-07-03 19:28:56 +0200

    update app.js to use catalog router

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
 
 const mongoDB =
   "mongodb+srv://abdogado:gedox23101997@cluster0.hexoi.mongodb.net/grocery-app?retryWrites=true&w=majority";
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.user('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
