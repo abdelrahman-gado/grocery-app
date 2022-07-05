c7a96bea45ac14477823c9f68769d4df8ca97712
abdelrahman <gadoua50@gmail.com>
2022-07-05 19:19:59 +0200

    define item_list function

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,7 +1,13 @@
 const Item = require('../models/item');
 
-exports.item_list = function (req, res) {
-  res.send('NOT IMPLEMENTED');
+exports.item_list = function (req, res, next) {
+  Item.find({}).populate('category').exec(function (err, result) {
+    if (err) {
+      return next(err);
+    }
+
+    res.render('all-items', { title: 'All Items', error: err, data: result });
+  })
 };
 
 exports.item_detail = function (req, res) {
