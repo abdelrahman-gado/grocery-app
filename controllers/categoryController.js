bf7c39834e579eddc933a7355eaf999cca6c6e62
abdelrahman <gadoua50@gmail.com>
2022-07-05 17:52:03 +0200

    define category_detail function

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,12 +3,22 @@ const Item = require("../models/item");
 
 exports.category_list = function (req, res) {
   Category.find({}, function (err, result) {
+    console.log(result);
     res.render("index", { title: "Grocery App", error: err, data: result });
   });
 };
 
-exports.category_detail = function (req, res) {
-  res.send("NOT IMPLEMNETED");
+exports.category_detail = function (req, res, next) {
+  const categoryId = req.params.id;
+  Item.find({ category: categoryId })
+    .populate("category")
+    .exec(function (err, result) {
+      if (err) {
+        return next(err);
+      }
+
+      res.render('category-detail', { title: result[0].category.name, data: result });
+    });
 };
 
 exports.category_create_get = function (req, res) {
