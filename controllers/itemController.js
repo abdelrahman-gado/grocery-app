9a9ed888801d4654f854fe67debd62792bf03c1d
abdelrahman <gadoua50@gmail.com>
2022-07-09 14:30:09 +0200

    define item_delete_post function in item controller

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,24 +1,32 @@
-const Item = require('../models/item');
+const Item = require("../models/item");
 
 exports.item_list = function (req, res, next) {
-  Item.find({}).populate('category').exec(function (err, result) {
-    if (err) {
-      return next(err);
-    }
+  Item.find({})
+    .populate("category")
+    .exec(function (err, result) {
+      if (err) {
+        return next(err);
+      }
 
-    res.render('all-items', { title: 'All Items', error: err, data: result });
-  })
+      res.render("all-items", { title: "All Items", error: err, data: result });
+    });
 };
 
 exports.item_detail = function (req, res, next) {
   const itemId = req.params.id;
-  Item.findById(itemId).populate('category').exec(function (err, result) {
-    if (err) {
-      return next(err);
-    }
+  Item.findById(itemId)
+    .populate("category")
+    .exec(function (err, result) {
+      if (err) {
+        return next(err);
+      }
 
-    res.render('item-detail', { itemName: result.name, error: err, data: result });
-  })
+      res.render("item-detail", {
+        itemName: result.name,
+        error: err,
+        data: result,
+      });
+    });
 };
 
 exports.item_create_get = function (req, res) {
@@ -33,10 +41,16 @@ exports.item_delete_get = function (req, res) {
   res.send("NOT IMPLEMENTED");
 };
 
-exports.item_delete_post = function (req, res) {
-  res.send("NOT IMPLEMENTED");
-};
+exports.item_delete_post = function (req, res, next) {
+  const itemId = req.params.id;
+  Item.deleteOne({ "_id" : itemId }).exec(function (err) {
+    if (err) {
+      return next(err);
+    }
 
+    res.redirect('/catalog/items');
+  });
+};
 
 exports.item_update_get = function (req, res) {
   res.send("NOT IMPLEMENTED");
@@ -45,10 +59,3 @@ exports.item_update_get = function (req, res) {
 exports.item_update_post = function (req, res) {
   res.send("NOT IMPLEMENTED");
 };
-
-
-
-
-
-
-
